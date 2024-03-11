78a7f32992f23e968641d55f813109589e4622f1
Manatoa <43567527+ManaToa@users.noreply.github.com>
2024-03-11 17:40:51 +0100

    Create index.tsx

---
+++ index.tsx
@@ -0,0 +1,29 @@
+import { useEffect, useState } from 'react'
+import { initRecaptcha } from '../../utils/reCaptcha'
+import BoxContainer from '../box-container'
+import ServerResponse from '../server-response'
+
+import CreateIssue from './create-issue'
+import DeleteIssue from './delete-issue'
+import GETExamples from './get-example'
+import UpdateIssue from './update-issue'
+
+export const BASE_URL = `/issue-tracker`
+
+export default function IssueTracker() {
+  const [serverResult, setServerResult] = useState<string>('')
+
+  useEffect(() => {
+    initRecaptcha()
+  }, [])
+
+  return (
+    <div className='w-[13.5rem] md:w-[33rem] xl:w-[29rem] 2xl:w-[35rem]'>
+      <BoxContainer children={<CreateIssue updateServerResponse={setServerResult} />} />
+      <BoxContainer children={<UpdateIssue updateServerResponse={setServerResult} />} />
+      <BoxContainer children={<DeleteIssue updateServerResponse={setServerResult} />} />
+      <BoxContainer children={<ServerResponse response={serverResult} />} />
+      <BoxContainer children={<GETExamples />} />
+    </div>
+  )
+}
